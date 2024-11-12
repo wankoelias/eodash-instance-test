b8deb04c29f46d332638ca3f7901f4f7f675a9fe
Daniel Santillan <daniel.santillan@eox.at>
2024-11-12 11:13:23 +0100

    chore: updating to latest eodash version

--- eodash.config.js
+++ eodash.config.js
@@ -4,4 +4,17 @@ export default defineConfig({
   dev: {
     port: 3000,
   },
+  vite:{
+    optimizeDeps: {
+      include: [
+        "@eox/map",
+        "@eox/map/src/plugins/advancedLayersAndSources",
+        "@eox/jsonform",
+        "@eox/layercontrol",
+        "@eox/itemfilter",
+        "@eox/timecontrol",
+        "@eox/stacinfo",
+      ],
+    },
+  }
 });
